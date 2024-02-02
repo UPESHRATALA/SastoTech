1cb6a76740d92631e4d856ae880c875f74f28926
UPESHRATALA <upesratala14@gmail.com>
2024-02-02 11:33:42 +0545

    added cart

--- website/models.py
+++ website/models.py
@@ -44,3 +44,29 @@ class Product(db.Model):
 
     def __str__(self):
         return '<Product %r>' % self.product_name
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    customer_link = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    product_link = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+    # customer product
+
+    def __str__(self):
+        return '<Cart %r>' % self.id
+
+
+
+
+
+
+
+
+
+
+
+
+
