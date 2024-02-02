8bc97b08f6c7825df377ccb9b810273bd81e2ad3
UPESHRATALA <upesratala14@gmail.com>
2024-02-02 11:37:14 +0545

    added cart route

--- website/views.py
+++ website/views.py
@@ -24,13 +24,12 @@ def home():
 
 
 
+@views.route('/cart')
+@login_required
+def show_cart():
+    cart = Cart.query.filter_by(customer_link=current_user.id).all()
+    amount = 0
+    for item in cart:
+        amount += item.product.current_price * item.quantity
 
-
-
-
-
-
-
-
-
-
+    return render_template('cart.html', cart=cart, amount=amount, total=amount+200)
