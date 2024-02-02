1b6313543f729c6c0d0db9e547679777aae0056b
UPESHRATALA <upesratala14@gmail.com>
2024-02-02 11:41:49 +0545

    added search routes

--- website/views.py
+++ website/views.py
@@ -192,6 +192,15 @@ def order():
     return render_template('orders.html', orders=orders)
 
 
+@views.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        search_query = request.form.get('search')
+        items = Product.query.filter(Product.product_name.ilike(f'%{search_query}%')).all()
+        return render_template('search.html', items=items, cart=Cart.query.filter_by(customer_link=current_user.id).all()
+                           if current_user.is_authenticated else [])
+
+    return render_template('search.html')
 
 
 
